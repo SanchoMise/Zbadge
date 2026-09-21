24f560c1b9340c59bff99aa7d9bb48cb7856206f
SanchoMise <bertrandsandrez@gmail.com>
2026-09-21 23:42:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker : cache l'app pour un affichage instantané (et hors-ligne)
-const CACHE = 'creche-v8';
+const CACHE = 'creche-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
